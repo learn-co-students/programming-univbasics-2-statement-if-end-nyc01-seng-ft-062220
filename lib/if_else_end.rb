0ac770c6fba395df82cdf24b6d25cf482cfa40cf
JULIA ZHOU <jzhouerm@hotmail.com>
2020-06-11 17:26:39 +0000

    Done.

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,1 +1,15 @@
-# Write your solution here
+#Solution 1
+current_time = Time.now 
+current_time = current_time.to_i 
+#if current_time % 2 == 0 
+ # puts "Even!"
+#else 
+ # puts "Odd!"
+#end 
+
+#Solution 2
+if current_time.even?
+  puts "Even!"
+else
+  puts "Odd!"
+end
